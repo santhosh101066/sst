7bbad2d9563a137321e01f312306875e0c75fb17
santhosh101066 <78214579+santhosh101066@users.noreply.github.com>
2025-06-03 22:20:00 +0530

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/vnd.apple.mpegurl");
 
-$api_url = "https://stalker.santhosh-home-101066.site/live.m3u8?cmd=ffrt%20http%3A%2F%2Flocalhost%2Fch%2F24059"; // API URL
+$api_url = "http://stalker.santhosh-home-101066.site/live.m3u8?cmd=ffrt%20http%3A%2F%2Flocalhost%2Fch%2F24059"; // API URL
 
 $m3u8_url = file_get_contents($api_url);
 
